f446c8b95766ac60eb3eabdfb4d7542dcf5f9a8b
Duncan Van Keulen <duncvan@gmail.com>
2023-10-17 17:02:49 -0400

    This is the setup that was causing GQL errors for me with our real implementation, however it's not giving the same errors here when queried

--- test/_community/collections/Posts/index.ts
+++ test/_community/collections/Posts/index.ts
@@ -20,5 +20,96 @@ export const PostsCollection: CollectionConfig = {
         update: () => false,
       },
     },
+    {
+      label: 'Alternative Content',
+      type: 'collapsible',
+      fields: [
+        {
+          name: 'alternative',
+          type: 'group',
+          admin: {
+            description:
+              'The alternative content of the feature. Will show below the call to action. Can either be a card or text.',
+          },
+          fields: [
+            {
+              name: 'variant',
+              type: 'select',
+              admin: {
+                isClearable: true,
+              },
+              options: [
+                {
+                  label: 'Card',
+                  value: 'card',
+                },
+
+                {
+                  label: 'Text',
+                  value: 'text',
+                },
+              ],
+            },
+
+            {
+              validate: (value, { siblingData }) => {
+                if (!value && siblingData?.variant) {
+                  return 'Relative Link URL (start with /) is required when a variant is selected'
+                }
+
+                return true
+              },
+              name: 'href',
+              label: 'href',
+              type: 'text',
+            },
+
+            {
+              name: 'linkText',
+              type: 'text',
+            },
+
+            {
+              name: 'detailText',
+              type: 'text',
+            },
+
+            {
+              name: 'image',
+              type: 'group',
+              admin: {
+                condition: (data) => {
+                  return data.alternative?.variant === 'card'
+                },
+              },
+              fields: [
+                {
+                  name: 'src',
+                  type: 'text',
+                  required: true,
+                },
+
+                {
+                  name: 'alt',
+                  type: 'text',
+                },
+
+                {
+                  name: 'width',
+                  type: 'number',
+                  required: true,
+                },
+
+                {
+                  name: 'height',
+                  type: 'number',
+                  required: true,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
   ],
 }
